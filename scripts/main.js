e60a9d1f18a0a65dc4bf6f0efa23ac2a8146e735
Claudio Torres <claudioricardotwf@gmail.com>
2020-06-07 01:21:47 -0300

    Removed focus when a button is clicked

--- scripts/main.js
+++ scripts/main.js
@@ -290,102 +290,159 @@ function getInput() {
         insertNumber(i);
       }
     });
-    document.getElementById(i).onclick = () => insertNumber(i);
-
     document.addEventListener("keyup", event => {
       if (event.key === `${i}`) document.getElementById(i).blur();
     });
+
+    document.getElementById(i).onclick = () => {
+      insertNumber(i);
+      document.getElementById(i).blur();
+    }
   }
 
   // Check for Dot input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === ".") document.getElementById("dot").focus();
+    if (event.key === ".") {
+      document.getElementById("dot").focus();
+      insertDot();
+    }
   });
-  document.getElementById("dot").onfocus = () => insertDot();
-
   document.addEventListener("keyup", event => {
     if (event.key === ".") document.getElementById("dot").blur();
   });
 
+  document.getElementById("dot").onclick = () => {
+    insertDot();
+    document.getElementById("dot").blur();
+  }
+
   // Get Plus sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "+") document.getElementById("plus").focus();
+    if (event.key === "+") {
+      document.getElementById("plus").focus();
+      insertSignOperator("+");
+    }
   });
-  document.getElementById("plus").onfocus = () => insertSignOperator("+");
-
   document.addEventListener("keyup", event => {
     if (event.key === "+") document.getElementById("plus").blur();
   });
 
+  document.getElementById("plus").onclick = () => {
+    insertSignOperator("+");
+    document.getElementById("plus").blur();
+  }
+
   // Get Minus sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "-") document.getElementById("minus").focus();
+    if (event.key === "-") {
+      document.getElementById("minus").focus();
+      insertSignOperator("-");
+    }
   });
-  document.getElementById("minus").onfocus = () => insertSignOperator("-");
-
   document.addEventListener("keyup", event => {
     if (event.key === "-") document.getElementById("minus").blur();
   });
 
+  document.getElementById("minus").onclick = () => {
+    insertSignOperator("-");
+    document.getElementById("minus").blur();
+  }
+
   // Get the multiply sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "*") document.getElementById("multiply").focus();
+    if (event.key === "*") {
+      document.getElementById("multiply").focus();
+      insertOperator("x");
+    }
   });
-  document.getElementById("multiply").onfocus = () => insertOperator("x");
-
   document.addEventListener("keyup", event => {
     if (event.key === "*") document.getElementById("multiply").blur();
   });
 
+  document.getElementById("multiply").onclick = () => {
+    insertOperator("x");
+    document.getElementById("multiply").blur();
+  }
+
   // Get the Divide sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "/") document.getElementById("divide").focus();
+    if (event.key === "/") {
+      document.getElementById("divide").focus();
+      insertOperator("/");
+    }
   });
-  document.getElementById("divide").onfocus = () => insertOperator("/");
-
   document.addEventListener("keyup", event => {
     if (event.key === "/") document.getElementById("divide").blur();
   });
 
+  document.getElementById("divide").onclick = () => {
+    insertOperator("/");
+    document.getElementById("divide").blur();
+  }
+
   // Get the Remain sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "%") document.getElementById("remain").focus();
+    if (event.key === "%") {
+      document.getElementById("remain").focus();
+      insertOperator("mod");
+    }
   });
-  document.getElementById("remain").onfocus = () => insertOperator("mod");
-
   document.addEventListener("keyup", event => {
     if (event.key === "%") document.getElementById("remain").blur();
   });
 
+  document.getElementById("remain").onclick = () => {
+    insertOperator("mod");
+    document.getElementById("remain").blur();
+  }
+
   // Get the Equals sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "Enter") document.getElementById("equals").focus();
+    if (event.key === "Enter") {
+      document.getElementById("equals").focus();
+      operate();
+    }
   });
-  document.getElementById("equals").onfocus = () => operate();
-
   document.addEventListener("keyup", event => {
     if (event.key === "Enter") document.getElementById("equals").blur();
   });
 
+  document.getElementById("equals").onclick = () => {
+    operate();
+    document.getElementById("equals").blur();
+  }
+
   // Get the Delete sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "Backspace") document.getElementById("delete").focus();
+    if (event.key === "Backspace") {
+      document.getElementById("delete").focus();
+      deleteChar();
+    }
   });
-  document.getElementById("delete").onfocus = () => deleteChar();
-
   document.addEventListener("keyup", event => {
     if (event.key === "Backspace") document.getElementById("delete").blur();
   });
 
+  document.getElementById("delete").onclick = () => {
+    deleteChar();
+    document.getElementById("delete").blur();
+  }
+
   // Get the Clear sign input, either by keyboard or by button click.
   document.addEventListener("keydown", event => {
-    if (event.key === "Escape") document.getElementById("clear").focus();
+    if (event.key === "Escape") {
+      document.getElementById("clear").focus();
+      clearDisplay();
+    }
   });
-  document.getElementById("clear").onfocus = () => clearDisplay();
-
   document.addEventListener("keyup", event => {
     if (event.key === "Escape") document.getElementById("clear").blur();
   });
+
+  document.getElementById("clear").onclick = () => {
+    clearDisplay();
+    document.getElementById("clear").blur();
+  }
 }
 
 getInput();
