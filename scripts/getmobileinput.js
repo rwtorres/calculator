a041bda3ba1880b36f56716712c6ed082b2e2520
Claudio Torres <claudioricardotwf@gmail.com>
2020-06-09 22:24:37 -0300

    Adjusted comments and made styles more similar

--- scripts/getmobileinput.js
+++ scripts/getmobileinput.js
@@ -2,54 +2,54 @@ getInput();
 
 // Check for user input on numbers, dots and operations.
 function getInput() {
-  // Check for Number input, either by keyboard or by button click.
+  // Check for Number input, by button touch.
   for (let i = 0; i < 10; i++) {
     document.getElementById(i).ontouchstart = () => {
       insertNumber(i);
     }
   }
 
-  // Check for Dot input, either by keyboard or by button click.
+  // Check for Dot input, by button touch.
   document.getElementById("dot").ontouchstart = () => {
     insertDot();
   }
 
-  // Get Plus sign input, either by keyboard or by button click.
+  // Get Plus sign input, by button touch.
   document.getElementById("plus").ontouchstart = () => {
     insertSignOperator("+");
   }
 
-  // Get Minus sign input, either by keyboard or by button click.
+  // Get Minus sign input, by button touch.
   document.getElementById("minus").ontouchstart = () => {
     insertSignOperator("-");
   }
 
-  // Get the multiply sign input, either by keyboard or by button click.
+  // Get the multiply sign input, by button touch.
   document.getElementById("multiply").ontouchstart = () => {
     insertOperator("×");
   }
 
-  // Get the Divide sign input, either by keyboard or by button click.
+  // Get the Divide sign input, by button touch.
   document.getElementById("divide").ontouchstart = () => {
     insertOperator("÷");
   }
 
-  // Get the Remain sign input, either by keyboard or by button click.
+  // Get the Remain sign input, by button touch.
   document.getElementById("remain").ontouchstart = () => {
     insertOperator("mod");
   }
 
-  // Get the Equals sign input, either by keyboard or by button click.
+  // Get the Equals sign input, by button touch.
   document.getElementById("equals").ontouchstart = () => {
     operate();
   }
 
-  // Get the Delete sign input, either by keyboard or by button click.
+  // Get the Delete sign input, by button touch.
   document.getElementById("delete").ontouchstart = () => {
     deleteChar();
   }
 
-  // Get the Clear sign input, either by keyboard or by button click.
+  // Get the Clear sign input, by button touch.
   document.getElementById("clear").ontouchstart = () => {
     clearDisplay();
   }
